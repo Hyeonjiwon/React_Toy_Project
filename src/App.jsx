e8d490b7df9edb4a39ec3eb97eb8da30faf2e8dc
Hyeonjiwon <zlone1208@gmail.com>
2024-05-16 21:31:23 +0900

    2024.05.16. prettierrc

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import { useState, useEffect } from "react";
-import store from "./Store.js";
-import Header from "./components/Header.js";
-import Table from "./components/Table.js";
+import { useState, useEffect } from 'react';
+import store from './Store.js';
+import Header from './components/Header.js';
+import Table from './components/Table.js';
 
 const App = () => {
-    const defaultSearchKeyword = "";
-    const [userData, setUserData] = useState([]);
-    const [countriesData, setCountriesData] = useState([]);
+  const defaultSearchKeyword = '';
+  const [userData, setUserData] = useState([]);
+  const [countriesData, setCountriesData] = useState([]);
 
-    const searchUserData = (defaultSearchKeyword) => {
-        const userData = store.search(defaultSearchKeyword, "userData");
-        setUserData(userData);
-    }
+  const searchUserData = (defaultSearchKeyword) => {
+    const userData = store.search(defaultSearchKeyword, 'userData');
+    setUserData(userData);
+  };
 
-    const searchCountriesData = (defaultSearchKeyword) => {
-        const countriesData = store.search(defaultSearchKeyword, "countriesData");
-        setCountriesData(countriesData);
-    }
+  const searchCountriesData = (defaultSearchKeyword) => {
+    const countriesData = store.search(defaultSearchKeyword, 'countriesData');
+    setCountriesData(countriesData);
+  };
 
-    // App 컴포넌트가 렌더링될 때 초기 검색을 실행
-    useEffect(() => {
-        searchUserData(defaultSearchKeyword);
-        searchCountriesData(defaultSearchKeyword);
-    }, []);
+  // App 컴포넌트가 렌더링될 때 초기 검색을 실행
+  useEffect(() => {
+    searchUserData(defaultSearchKeyword);
+    searchCountriesData(defaultSearchKeyword);
+  }, []);
 
-    return (
-        <>
-            <Header title="헤더" />
-            <Table title="User" data={userData}/>
-            <Table title="Country" data={countriesData}/>
-        </>
-    );
-}
+  return (
+    <>
+      <Header title="헤더" />
+      <div>TODO: 콤보 | 인풋 | 버튼 </div>
+      <></>
+      <Table title="User" data={userData} />
+      <Table title="Country" data={countriesData} />
+    </>
+  );
+};
 
 export default App;
